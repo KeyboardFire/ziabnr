297e8d8d5921cbe22bf0ee5767caa7c96e2bbe16
KeyboardFire <andy@keyboardfire.com>
2016-10-07 21:36:26 -0500

    Implement player movement
    Movement is with hjklyubn, like in NetHack.

--- src/main.rs
+++ src/main.rs
@@ -28,6 +28,7 @@ struct MapTile {
 trait Object {
     fn get_disp(&self) -> Disp;
     fn get_pos(&self) -> Pos;
+    fn turn(&mut self);
 }
 
 struct Player {
@@ -41,44 +42,78 @@ impl Object for Player {
     fn get_pos(&self) -> Pos {
         self.pos
     }
+
+    fn turn(&mut self) {
+        let ch = ncurses::getch() as u8 as char;
+        if ch == 'h' || ch == 'y' || ch == 'b' { self.pos.col -= 1; }
+        if ch == 'j' || ch == 'b' || ch == 'n' { self.pos.row += 1; }
+        if ch == 'k' || ch == 'y' || ch == 'u' { self.pos.row -= 1; }
+        if ch == 'l' || ch == 'u' || ch == 'n' { self.pos.col += 1; }
+    }
 }
 
 fn main() {
-    let map = unsafe {
+    let mut map = unsafe {
         let mut map: [[MapTile; 77]; 19] = ::std::mem::uninitialized();
         for row in map.iter_mut() {
             for x in row.iter_mut() {
                 ::std::ptr::write(x, MapTile { disp: Disp {
-                    ch: rand::thread_rng().gen_ascii_chars().next().unwrap(),
-                    color: rand::thread_rng().gen_range(1, 8)
+                    ch: ' ',
+                    color: ncurses::COLOR_WHITE
                 }});
             }
         }
         map
     };
 
+    // draw a simple room
+    for row in 3..13 {
+        map[row][3] = MapTile { disp: Disp {
+            ch: '|',
+            color: ncurses::COLOR_WHITE
+        }};
+        map[row][13] = MapTile { disp: Disp {
+            ch: '|',
+            color: ncurses::COLOR_WHITE
+        }};
+    }
+    for col in 3..14 {
+        map[3][col] = MapTile { disp: Disp {
+            ch: '-',
+            color: ncurses::COLOR_WHITE
+        }};
+        map[13][col] = MapTile { disp: Disp {
+            ch: '-',
+            color: ncurses::COLOR_WHITE
+        }};
+    }
+
     let mut objects: Vec<Box<Object>> = Vec::new();
     objects.push(Box::new(Player { pos: Pos { row: 5, col: 5 }}));
 
     ncurses::initscr();
+    ncurses::noecho();
     ncurses::start_color();
     for i in 1i16..8i16 {
         // red, green, yellow, blue, magenta, cyan, white
         ncurses::init_pair(i, i, ncurses::COLOR_BLACK);
     }
 
-    for (i, row) in map.iter().enumerate() {
-        for (j, tile) in row.iter().enumerate() {
-            tile.disp.draw(&Pos { row: i as i32, col: j as i32 });
+    loop {
+        for mut object in objects.iter_mut() {
+            object.turn();
         }
+        for (i, row) in map.iter().enumerate() {
+            for (j, tile) in row.iter().enumerate() {
+                tile.disp.draw(&Pos { row: i as i32, col: j as i32 });
+            }
+        }
+        for object in objects.iter().rev() {
+            object.get_disp().draw(&object.get_pos());
+        }
+        ncurses::addch(8); // backspace; put cursor on top of player
+        ncurses::refresh();
     }
-    for object in objects.iter().rev() {
-        object.get_disp().draw(&object.get_pos());
-    }
-    ncurses::addch(8); // backspace; put cursor on top of player
 
-    ncurses::refresh();
-
-    ncurses::getch();
     ncurses::endwin();
 }
