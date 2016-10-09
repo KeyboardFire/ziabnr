092553def62a52f0d1edbac1414311349716780c
KeyboardFire <andy@keyboardfire.com>
2016-10-08 23:33:26 -0500

    Implement simple dungeon generation
    It makes a bunch of rooms.
    
    Right now there are no corriders. Also, things can spawn inside walls.
    
    Also, corners of rooms are wonky, but that will be fixed in the next
    commit hopefully.

--- src/main.rs
+++ src/main.rs
@@ -135,6 +135,20 @@ fn move_relative(object: &Object, offset: &Pos, map: &Map,
     }
 }
 
+struct Room {
+    pos: Vec2,
+    repulsion: Vec2,
+    width: f32,
+    height: f32
+}
+
+struct Vec2 {
+    x: f32, y: f32
+}
+
+const REPULSION_FACTOR: f32 = 0.000001;
+const REPULSION_TRIES: usize = 10000;
+
 fn main() {
     let mut map = unsafe {
         let mut map: Map = ::std::mem::uninitialized();
@@ -146,14 +160,68 @@ fn main() {
         map
     };
 
-    // draw a simple room
-    for row in 3..13 {
-        map[row][3] = Box::new(VertWall {});
-        map[row][13] = Box::new(VertWall {});
+    let mut rooms: Vec<Room> = (0..20).map(|_| Room {
+        pos: Vec2 {
+            x: rand::thread_rng().gen_range(5.0, 72.0),
+            y: rand::thread_rng().gen_range(3.0, 16.0)
+        },
+        repulsion: Vec2 { x: 0.0, y: 0.0 },
+        width: rand::thread_rng().gen_range(2.0, 5.0),
+        height: rand::thread_rng().gen_range(2.0, 3.5)
+    }).collect();
+
+    for _ in 0..REPULSION_TRIES {
+        for i in 0..rooms.len() {
+            for j in i+1..rooms.len() {
+                let repulsion = Vec2 {
+                    x: 1.0 / (rooms[i].pos.x - rooms[j].pos.x).powi(5),
+                    y: 1.0 / (rooms[i].pos.y - rooms[j].pos.y).powi(5)
+                };
+                rooms[i].repulsion.x += rooms[j].width  * repulsion.x;
+                rooms[i].repulsion.y += rooms[j].height * repulsion.y;
+                rooms[j].repulsion.x += rooms[i].width  * repulsion.x;
+                rooms[j].repulsion.y += rooms[i].height * repulsion.y;
+            }
+        }
+
+        for mut room in rooms.iter_mut() {
+            room.pos.x += room.repulsion.x * REPULSION_FACTOR;
+            if room.pos.x < room.width { room.pos.x = room.width; }
+            if room.pos.x > 77.0 - room.width { room.pos.x = 77.0 - room.width; }
+
+            room.pos.y += room.repulsion.y * REPULSION_FACTOR;
+            if room.pos.y < room.height { room.pos.y = room.height; }
+            if room.pos.y > 19.0 - room.height { room.pos.y = 19.0 - room.height; }
+
+            room.repulsion = Vec2 { x: 0.0, y: 0.0 };
+        }
     }
-    for col in 3..14 {
-        map[3][col] = Box::new(HorizWall {});
-        map[13][col] = Box::new(HorizWall {});
+
+    let mut data: [[i32; 77]; 19] = [[0; 77]; 19];
+    for room in rooms.iter() {
+        for col in (room.pos.x - room.width).round() as usize..(room.pos.x + room.width).round() as usize {
+            for row in (room.pos.y - room.height).round() as usize..(room.pos.y + room.height).round() as usize {
+                data[row][col] = 1;
+            }
+        }
+    }
+    for row in 0..19 {
+        for col in 0..77 {
+            if data[row][col] == 1 {
+                if row != 0 && data[row-1][col] == 1 &&
+                   col != 0 && data[row][col-1] == 1 &&
+                   row != 19-1 && data[row+1][col] == 1 &&
+                   col != 77-1 && data[row][col+1] == 1 {
+                    continue;
+                }
+                if row == 0 || row == 19-1 ||
+                   data[row-1][col] != 1 || data[row+1][col] != 1 {
+                    map[row][col] = Box::new(HorizWall {});
+                } else {
+                    map[row][col] = Box::new(VertWall {});
+                }
+            }
+        }
     }
 
     let mut objects: Vec<Box<Object>> = Vec::new();
