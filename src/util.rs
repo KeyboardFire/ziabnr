ae58c7371eef4e9efe792808fe07039658e350bc
KeyboardFire <andy@keyboardfire.com>
2016-10-14 06:31:05 -0500

    Check bounds in move_relative()
    So that we don't crash when anything tries to move offscreen.

--- src/util.rs
+++ src/util.rs
@@ -6,7 +6,8 @@ pub fn move_relative(object: &Object, offset: &Pos, map: &Map,
                      before: &[Box<Object>], after: &[Box<Object>]) -> Option<Pos> {
     let to = Pos { row: object.get_pos().row + offset.row,
                    col: object.get_pos().col + offset.col };
-    if map[to.row as usize][to.col as usize].passable(object) &&
+    if to.row >= 0 && to.row < 77 && to.col >= 0 && to.col < 19 &&
+            map[to.row as usize][to.col as usize].passable(object) &&
             before.iter().chain(after.iter()).all(|obj|
                 obj.get_pos() != to || obj.passable(object)) {
         Some(to)
